79a8d6675b22e90ab84986050c268d8e6cded96e
gkarumba <gachegua@gmail.com>
2019-06-02 19:51:47 +0300

    Add the quote-details functionality

--- src/app/components/quote-details/quote-details.component.ts
+++ src/app/components/quote-details/quote-details.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../../models/quote';
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteDetailsComponent implements OnInit {
 
+  @Input () quote: Quote;
+  @Output () isToDelete = new EventEmitter<boolean>();
+
+  deleteQuote(quoteDeleted: boolean) {
+    this.isToDelete.emit(quoteDeleted);
+  }
+  addUpVote() {
+    this.quote.upVote ++;
+  }
+
+  addDownVote() {
+    this.quote.downVote ++;
+  }
   constructor() { }
 
   ngOnInit() {
